0d7b454363210ea24c6c2dd15e95cdb0f3083200
Sentinelle <cp.chris.trading@gmail.com>
2024-10-23 09:56:11 +0200

    Aggiunto variabili email e message, aggiunto controllo campi e richiamo modifica css

--- script.js
+++ script.js
@@ -2,19 +2,39 @@
 
 const form = document.querySelector("form");
 const nameInput = document.querySelector("#name"); // si usa # perchè si prende il nome id
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
 const messageSection = document.createElement("p"); // creiamo un elemento <p> per il messaggio
 
+// inserisce un event listener al pulsante di invio
 form.addEventListener("submit", function (event) {
-  // inserisce un event listener al pulsante di invio
   event.preventDefault(); // previene il comportamento di defalt del form, cioè ricaricare la pagina
 
-  const userName = nameInput.value; // ottiene il valore dell'input "name"
+  // otteniamo i valori degli input
+  const userName = nameInput.value.trim(); // .tri() toglie gli spazi vuoti
+  const userEmail = emailInput.value.trim();
+  const userMessage = messageInput.value.trim();
 
-  // creiamo un messaggio personalizzato
-  const message = `Grazie ${userName} per averci contattato!`;
+  // validazione personalizzata: controlliamo che i campi non siano vuoti
+  if (!userName || !userEmail || !userMessage) {
+    messageSection.textContent = `Tutti i campi sono obbligatori.`;
+    messageSection.classList.remove("success");
+    messageSection.classList.add("error");
+  } else {
+    // se tutto è corretto mostriamo un messaggio di conferma
+    messageSection.textContent = `Grazie ${userName} per averci contattato, ti risponderemo presto via mail a ${userEmail}.`;
+    messageSection.classList.remove("error");
+    messageSection.classList.add("success");
 
-  // aggiungiamo il messaggio al DOM
-  messageSection.textContent = message;
+    // opzionale, resetta i campi del form
+    form.reset;
+  }
+
+  // // creiamo un messaggio personalizzato
+  // const message = `Grazie ${userName} per averci contattato!`;
+
+  // // aggiungiamo il messaggio al DOM
+  // messageSection.textContent = message;
 });
 
 /* aggiungi l'elemento p come figlio del form */
