8422fc233894b974b67f281cdbddd8b5da9a6b60
Sentinelle <cp.chris.trading@gmail.com>
2024-10-24 08:52:24 +0200

    inizio giorno 3

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// seleziona il form e aggiunge uun event listener per l'invio
+// seleziona il form e aggiunge un event listener per l'invio
 
 const form = document.querySelector("form");
-const nameInput = document.querySelector("#name"); // si usa # perchè si prende il nome id
+const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
-const messageSection = document.createElement("p"); // creiamo un elemento <p> per il messaggio
+const messageSection = document.createElement("p");
 
 // validazione email
 function validateEmail(userEmail) {
@@ -14,34 +14,29 @@ function validateEmail(userEmail) {
 
 // inserisce un event listener al pulsante di invio
 form.addEventListener("submit", function (event) {
-  event.preventDefault(); // previene il comportamento di defalt del form, cioè ricaricare la pagina
+  event.preventDefault(); // previene il comportamento di default del form
 
-  // otteniamo i valori degli input
-  const userName = nameInput.value.trim(); // .tri() toglie gli spazi vuoti
+  const userName = nameInput.value.trim();
   const userEmail = emailInput.value.trim();
   const userMessage = messageInput.value.trim();
 
-  // validazione personalizzata: controlliamo che i campi non siano vuoti
   if (!userName || !userEmail || !userMessage) {
     messageSection.textContent = `Tutti i campi sono obbligatori.`;
-    messageSection.classList.remove("success"); // modifica lo stile
-    messageSection.classList.add("error"); // modifica lo stile
-  }
-  // validazione dell'email
-  else if (!validateEmail(userEmail)) {
+    messageSection.classList.remove("success");
+    messageSection.classList.add("error");
+  } else if (!validateEmail(userEmail)) {
     messageSection.textContent = "Per favore inserire una email valida.";
-    messageSection.classList.remove("success"); // modifica lo stile
-    messageSection.classList.add("error"); // modifica lo stile
+    messageSection.classList.remove("success");
+    messageSection.classList.add("error");
   } else {
-    // se tutto è corretto mostriamo un messaggio di conferma
     messageSection.textContent = `Grazie ${userName} per averci contattato, ti risponderemo presto via mail a ${userEmail}.`;
-    messageSection.classList.remove("error"); // modifica lo stile
-    messageSection.classList.add("success"); // modifica lo stile
+    messageSection.classList.remove("error");
+    messageSection.classList.add("success");
 
     // opzionale, resetta i campi del form
     form.reset();
   }
 });
 
-/* aggiungi l'elemento p come figlio del form */
+// aggiungi l'elemento p come figlio del form
 form.appendChild(messageSection);
